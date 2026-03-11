cfe031732be9f1744de5d02fb376e56a56da9c69
Sunny Sahu <sunnysahu8210@gmail.com>
2026-03-11 12:15:24 +0530

    Adjusted Little

--- Program.cs
+++ Program.cs
@@ -25,12 +25,22 @@ namespace Learn_LINQ
                 new Student{ Id = 4, Name="Neha", Age=22, Marks=70 },
                 new Student{ Id = 5, Name="Priya", Age=19, Marks=85 }
             };
+
+            // LINQ Query Syntax - In query syntax, we use a SQL-like syntax to write LINQ queries. It is more readable and easier to understand for those familiar with SQL.
+
+            // LINQ Method Syntax - In method syntax, we use extension methods to write LINQ queries. It is more concise and allows for more complex queries.
+
+            // LINQ Query + Method Syntax - We can also combine both query syntax and method syntax in a single LINQ query. This allows us to take advantage of the readability of query syntax while also using the power of method syntax for more complex operations.
+            
+            
+            // Query Syntax : Get students age > 21
+
+
         }
 
-        // LINQ Query Syntax - In query syntax, we use a SQL-like syntax to write LINQ queries. It is more readable and easier to understand for those familiar with SQL.
 
-        // LINQ Method Syntax - In method syntax, we use extension methods to write LINQ queries. It is more concise and allows for more complex queries.
 
-        // LINQ Query + Method Syntax - We can also combine both query syntax and method syntax in a single LINQ query. This allows us to take advantage of the readability of query syntax while also using the power of method syntax for more complex operations.
+
+
     }
 }
